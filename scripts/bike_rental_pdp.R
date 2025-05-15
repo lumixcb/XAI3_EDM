f6b50845a98fb2485719a8d43469975090a9675d
Luminița Ciobanu Borinschi <157851154+lumixcb@users.noreply.github.com>
2025-05-15 18:48:33 +0200

    Update bike_rental_pdp.R

--- scripts/bike_rental_pdp.R
+++ scripts/bike_rental_pdp.R
@@ -1,1 +1,25 @@
+# Cargar librerías necesarias
+library(randomForest)
+library(pdp)
+library(ggplot2)
+library(dplyr)
+
+# Cargar los datos
+bike_data <- read.csv("../data/day.csv")
+
+# Entrenar el modelo de bosque aleatorio
+set.seed(123)
+bike_rf <- randomForest(cnt ~ yr + temp + hum + windspeed, data = bike_data, ntree = 100)
+
+# PDP para las cuatro variables
+variables <- c("yr", "temp", "hum", "windspeed")
+for (var in variables) {
+  pd <- partial(bike_rf, pred.var = var, grid.resolution = 100)
+  plot <- autoplot(pd) +
+    ggtitle(paste("PDP for", var)) +
+    theme_minimal()
+  
+  # Guardar gráfico como PNG
+  ggsave(filename = paste0("../reports/PDP_", var, ".png"), plot = plot, width = 8, height = 6)
+}
 
